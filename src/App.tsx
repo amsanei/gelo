5852e9258c1df3006dc7eb7e5de45b1a63c8b9d2
Amir mohammad <am.sanei79@gmail.com>
2025-05-19 16:51:29 +0330

    Add react query

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,28 @@
+import {
+  QueryClient,
+  QueryClientProvider,
+  useQuery,
+} from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
+
 function App() {
-  return <div className="bg-yellow-500 p-8">Hello, World</div>;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Furnitures />
+    </QueryClientProvider>
+  );
+}
+
+function Furnitures() {
+  const getData = async () => {
+    const res = await fetch("https://dummyjson.com/products?limit=10&skip=10");
+    return await res.json()
+  };
+  const {data, isLoading} = useQuery({ queryKey: ["data"], queryFn: getData });
+  if (isLoading) return <div>Wait...</div>
+  
+  return <div>{data.products.map((item : any) => <div key={item.id}>{item.title}</div>)}</div>;
 }
 
 export default App;
