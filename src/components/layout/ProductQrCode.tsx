2d45ee731179b1e1f52a9238bef949c5ea647b5e
Amir mohammad <am.sanei79@gmail.com>
2025-05-21 02:14:16 +0330

    Fix Bug

--- src/components/layout/ProductQrCode.tsx
+++ src/components/layout/ProductQrCode.tsx
@@ -12,8 +12,12 @@ export default function ProductQrCode({
   if (showQrCode)
     return (
       <div className="flex items-center justify-center w-full h-full top-0 left-0 absolute bg-black/20 backdrop-blur">
-        <div className="lg:w-2/3 bg-white p-2">
-          <img src={qrCode} alt="bar code" className="w-full" />
+        <div className="w-2/3 bg-white p-2 m-4">
+          <img
+            src={qrCode}
+            alt="bar code"
+            className="w-full max-w-32 max-h-32 mx-auto"
+          />
           <div className="flex flex-col items-center mt-4">
             <div>{barCode} </div>
             <div className="text-neutral-500">Bar Code</div>
