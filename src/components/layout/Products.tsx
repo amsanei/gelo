e15bd1f535db75a896bceea421dd733678cdf8cd
Amir mohammad <am.sanei79@gmail.com>
2025-05-20 18:18:17 +0330

    Fix bug and disable react query retry and refetch on window focus

--- src/components/layout/Products.tsx
+++ src/components/layout/Products.tsx
@@ -7,14 +7,16 @@ import ArrowRight from "../icons/ArrowRight";
 import ArrowLeft from "../icons/ArrowLeft";
 import axios from "../../axios/axios";
 import Button from "../ui/Button";
-import type {  ProductDetailData } from "../../types";
+import type { ProductDetailData } from "../../types";
 
 export default function Products() {
   const [currPage, setCurrPage] = useState(1);
   const limit = 8;
   const skip = (currPage - 1) * limit;
   const [isModalOpen, setIsModalOpen] = useState(false);
-  const [selectedItem, setSelectedItem] = useState<ProductDetailData | null>(null);
+  const [selectedItem, setSelectedItem] = useState<ProductDetailData | null>(
+    null
+  );
   const getData = async ({
     queryKey,
   }: {
@@ -28,6 +30,8 @@ export default function Products() {
   const { data, isLoading, isRefetching, isError, refetch } = useQuery({
     queryKey: ["data", limit, skip],
     queryFn: getData,
+    refetchOnWindowFocus: false,
+    retry: false,
   });
 
   return (
@@ -39,7 +43,7 @@ export default function Products() {
         )}
       </div>
       <div className="grid md:grid-cols-12 gap-6">
-        {isLoading || isRefetching ? (
+        {isLoading || isRefetching? (
           [1, 2, 3, 4, 5, 6, 7, 8].map((item) => (
             <div className="flex flex-col gap-4 col-span-3" key={item}>
               <div className="w-full aspect-square bg-neutral-200 animate-pulse"></div>
@@ -50,52 +54,56 @@ export default function Products() {
           ))
         ) : isError ? (
           <div className="col-span-12 text-center">
-            <div className="text-2xl mb-4">Something went wrong :(</div>
+            <div className="text-xl font-bold mb-2">Something went wrong :(</div>
+            <div className=" text-neutral-500 mb-4">Try with Vpn</div>
             <Button onClick={refetch}>Try Again</Button>
           </div>
         ) : (
-          data.products.map((item: ProductDetailData) => (
-            <div
-              className="cursor-pointer col-span-3"
-              onClick={() => {
-                setIsModalOpen(true);
-                setSelectedItem(item);
-              }}
-            >
-              <Card key={item.id} data={item} />
-            </div>
-          ))
+          <>
+            {data.products.map((item: ProductDetailData) => (
+              <div
+                className="cursor-pointer col-span-3"
+                onClick={() => {
+                  setIsModalOpen(true);
+                  setSelectedItem(item);
+                }}
+              >
+                <Card key={item.id} data={item} />
+              </div>
+            ))}
+            {!isLoading && (
+              <div className="text-sm  flex justify-between items-center mt-4 pt-2 border-t border-neutral-200">
+                <div className="text-neutral-500">
+                  Page {currPage} From {Math.ceil(data.total / limit)}
+                </div>
+                <div className="flex gap-4 md:gap-8">
+                  <button
+                    className="disabled:cursor-not-allowed disabled:text-neutral-500 cursor-pointer group flex items-start gap-2"
+                    onClick={() => setCurrPage((prev) => Math.max(prev - 1, 1))}
+                    disabled={currPage === 1}
+                  >
+                    <div className="group-hover:-translate-x-1 group-disabled:translate-x-0 transition-all">
+                      <ArrowLeft />
+                    </div>
+                    <div>Previous Page</div>
+                  </button>
+                  <button
+                    className="disabled:cursor-not-allowed disabled:text-neutral-500 cursor-pointer group flex items-start gap-2"
+                    onClick={() => setCurrPage((prev) => prev + 1)}
+                    disabled={skip + limit >= data.total}
+                  >
+                    <div>Next Page</div>
+                    <div className="group-hover:translate-x-1 group-disabled:translate-x-0 transition-all">
+                      <ArrowRight />
+                    </div>
+                  </button>
+                </div>
+              </div>
+            )}
+          </>
         )}
       </div>
-      {!isLoading && (
-        <div className="text-sm  flex justify-between items-center mt-4 pt-2 border-t border-neutral-200">
-          <div className="text-neutral-500">
-            Page {currPage} From {Math.ceil(data.total / limit)}
-          </div>
-          <div className="flex gap-4 md:gap-8">
-            <button
-              className="disabled:cursor-not-allowed disabled:text-neutral-500 cursor-pointer group flex items-start gap-2"
-              onClick={() => setCurrPage((prev) => Math.max(prev - 1, 1))}
-              disabled={currPage === 1}
-            >
-              <div className="group-hover:-translate-x-1 group-disabled:translate-x-0 transition-all">
-                <ArrowLeft />
-              </div>
-              <div>Previous Page</div>
-            </button>
-            <button
-              className="disabled:cursor-not-allowed disabled:text-neutral-500 cursor-pointer group flex items-start gap-2"
-              onClick={() => setCurrPage((prev) => prev + 1)}
-              disabled={skip + limit >= data.total}
-            >
-              <div>Next Page</div>
-              <div className="group-hover:translate-x-1 group-disabled:translate-x-0 transition-all">
-                <ArrowRight />
-              </div>
-            </button>
-          </div>
-        </div>
-      )}
+
       {selectedItem && (
         <Modal isOpen={isModalOpen} onClose={() => setIsModalOpen(false)}>
           <ProductDetail data={selectedItem} />
